1e3561dc45a5d2da4d10874e7abba375cbe3f3d3
miri <mirif@wix.com>
2017-07-23 14:59:15 +0300

    ## Stocks List - Phase 2 ## Hide reordering controls when the panel is shown Revert changes when hiding the filter panel Apply CSS to fit the mock

--- main.js
+++ main.js
@@ -2,136 +2,165 @@
  * Created by mirif on 18/07/2017.
  */
 
-//TODO - create app model object - what is the view made of
+//TODO - create app model object - what is the view made of (mvc)
 
-let stocksOrder = [
-  "WIX",
-  "MSFT",
-  "YHOO"
-];
+(function () {
+  'use strict';
 
-let Stocks = [
-  {
-    "Symbol": "WIX",
-    "Name": "Wix.com Ltd.",
-    "Change": "0.750000",
-    "PercentChange": "+1.51%",
-    "LastTradePriceOnly": "76.099998"
-  },
-  {
-    "Symbol": "MSFT",
-    "Name": "Microsoft Corporation",
-    "PercentChange": "-2.09%",
-    "Change": "-0.850006",
-    "LastTradePriceOnly": "69.620003"
-  },
-  {
-    "Symbol": "YHOO",
-    "Name": "Yahoo! Inc.",
-    "Change": "0.279999",
-    "PercentChange": "+1.11%",
-    "LastTradePriceOnly": "50.599998"
+  // define closest for unsupported browsers
+  if (window.Element && !Element.prototype.closest) {
+    Element.prototype.closest =
+      function(s) {
+        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+          i,
+          el = this;
+        do {
+          i = matches.length;
+          while (--i >= 0 && matches.item(i) !== el) {};
+        } while ((i < 0) && (el = el.parentElement));
+        return el;
+      };
   }
-];
+
+  let stocksOrder = [
+    "WIX",
+    "MSFT",
+    "YHOO"
+  ];
+
+  let Stocks = [
+    {
+      "Symbol": "WIX",
+      "Name": "Wix.com Ltd.",
+      "Change": "0.750000",
+      "PercentChange": "+1.51%",
+      "LastTradePriceOnly": "76.099998"
+    },
+    {
+      "Symbol": "MSFT",
+      "Name": "Microsoft Corporation",
+      "PercentChange": "-2.09%",
+      "Change": "-0.850006",
+      "LastTradePriceOnly": "69.620003"
+    },
+    {
+      "Symbol": "YHOO",
+      "Name": "Yahoo! Inc.",
+      "Change": "0.279999",
+      "PercentChange": "+1.11%",
+      "LastTradePriceOnly": "50.599998"
+    }
+  ];
 
 
-const VIEW_STATES = {
-  DAILY_CHANGE : 'DAILY_CHANGE',
-  MARKET_CAPITAL : 'MARKET_CAPITAL'
-};
+  const VIEW_STATES = {
+    DAILY_CHANGE : 'DAILY_CHANGE',
+    MARKET_CAPITAL : 'MARKET_CAPITAL'
+  };
 
-let stocksViewState;
-let filterState;
+  let stocksViewState;
 
-function init() {
-  stocksViewState = VIEW_STATES.DAILY_CHANGE;
-  filterState = VIEW_STATES.HIDE_FILTER;
-  initStockContainer();
-  const header = document.querySelector('header');
-  header.addEventListener('click', handleClickOnHeader);
-}
+  function init() {
+    stocksViewState = VIEW_STATES.DAILY_CHANGE;
+    initStockContainer();
+    const header = document.querySelector('header');
+    header.addEventListener('click', headerClickHandler);
+  }
 
-function handleClickOnHeader(e) {
-  if (e.target.dataset.id === 'filter-btn'){
-    const filterSection = document.querySelector('.filter-section');
-    filterSection.style.display = filterSection.style.display === 'none' ? 'block' : 'none';
-    if (filterSection.style.display === 'block') {
-      document.querySelector('.filter-btn').style.color='#41bf15';
-    } else {
-      document.querySelector('.filter-btn').style.color='#ababab';
+  function headerClickHandler(e) {
+    const target = e.target;
+    if (target.dataset.id === 'filter-btn'){
+      const filterSection = document.querySelector('.filter-section');
+      const upDownWrapperAll = document.querySelectorAll('.up-down-wrapper');
+      filterSection.style.display = filterSection.style.display === 'none' ? 'block' : 'none';
+      if (filterSection.style.display === 'block') {
+        document.querySelector('.filter-btn').style.color='#41bf15';
+        [].forEach.call(upDownWrapperAll, function (upDownWrapper) {
+          upDownWrapper.style.display = 'none';
+        });
+      } else {
+        document.querySelector('.filter-btn').style.color='#ababab';
+        [].forEach.call(upDownWrapperAll, function (upDownWrapper) {
+          upDownWrapper.style.display = 'flex';
+        });
+        initStocksList();
+      }
     }
   }
-}
 
-function handleClickOnStockListContainer(e) {
+  function stockListContainerClickHandler(e) {
+    const target = e.target;
 
-  if (e.target.dataset.id === 'stock-data-btn') {
-    stocksViewState = stocksViewState === VIEW_STATES.DAILY_CHANGE ? VIEW_STATES.MARKET_CAPITAL : VIEW_STATES.DAILY_CHANGE;
-    initStocksList();
+    if (target.dataset.id === 'stock-data-btn') {
+      stocksViewState = stocksViewState === VIEW_STATES.DAILY_CHANGE ? VIEW_STATES.MARKET_CAPITAL : VIEW_STATES.DAILY_CHANGE;
+      initStocksList();
+    }
+
+    if (target.dataset.id === 'nav-btn') {
+      const currLocation = stocksOrder.indexOf(target.closest('li').dataset.symbol);
+      const shouldStockMoveUp = target.dataset.direction === 'up';
+      const newLocation = shouldStockMoveUp ? currLocation - 1 : currLocation + 1;
+      swapStocksOrder(currLocation, newLocation);
+      initStocksList();
+    }
   }
 
-  if (e.target.dataset.id === 'nav-btn') {
-    const currLocation = stocksOrder.indexOf(e.target.parentNode.dataset.symbol);
-    const shouldStockMoveUp = e.target.dataset.direction === 'up';
-    const newLocation = shouldStockMoveUp ? currLocation - 1 : currLocation + 1;
-    stocksOrderChange(currLocation, newLocation);
-    initStocksList();
+  function swapStocksOrder(currLoc, newLoc) {
+    if (newLoc >= 0 && newLoc < stocksOrder.length) {
+      const temp = stocksOrder[newLoc];
+      stocksOrder[newLoc] = stocksOrder[currLoc];
+      stocksOrder[currLoc] = temp;
+    }
   }
 
-}
-
-function stocksOrderChange(currLoc, newLoc) {
-  if (newLoc >= 0 && newLoc < stocksOrder.length) {
-    const temp = stocksOrder[newLoc];
-    stocksOrder[newLoc] = stocksOrder[currLoc];
-    stocksOrder[currLoc] = temp;
-  }
-}
-
-function addStocksByOrder() {
-  let stockListHtml = '';
-  stocksOrder.forEach((stockSymbol) => {
-    let currStock = Stocks.find((stock) => {
-      return stock.Symbol === stockSymbol;
+  function addStocksByOrder() {
+    let stockListHtml = '';
+    stocksOrder.forEach((stockSymbol) => {
+      let currStock = Stocks.find((stock) => {
+        return stock.Symbol === stockSymbol;
+      });
+      stockListHtml += renderStock(currStock);
     });
-    stockListHtml += renderStock(currStock);
-  });
-  return stockListHtml;
-}
+    return stockListHtml;
+  }
 
-function initStockContainer() {
-  const stocksListWrapperDiv = document.querySelector('.stocks-list-wrapper');
-  stocksListWrapperDiv.addEventListener('click', handleClickOnStockListContainer);
-  initStocksList();
-}
-function initStocksList() {
-  const stocksListWrapperDiv = document.querySelector('.stocks-list-wrapper');
-  stocksListWrapperDiv.innerHTML = '<ul class="stocks-list">' + addStocksByOrder() + '</ul>';
-  disableButtons();
-}
+  function initStockContainer() {
+    const stocksListWrapperDiv = document.querySelector('.stocks-list-wrapper');
+    stocksListWrapperDiv.addEventListener('click', stockListContainerClickHandler);
+    initStocksList();
+  }
 
-function renderStock(stock) {
-  let btnClass = parseFloat(stock.PercentChange) > 0 ? 'btn-green' : 'btn-red';
-  let btnData = stocksViewState === VIEW_STATES.DAILY_CHANGE ? stock.PercentChange : parseFloat(stock.Change).toFixed(2);
-  return `<li class="stock" data-symbol="${stock.Symbol}">
+  function initStocksList() {
+    const stocksListWrapperDiv = document.querySelector('.stocks-list-wrapper');
+    stocksListWrapperDiv.innerHTML = '<ul class="stocks-list">' + addStocksByOrder() + '</ul>';
+    disableButtons();
+  }
+
+  function disableButtons() {
+    document.querySelector('.stock:first-child .btn-up').disabled = true;
+    document.querySelector('.stock:last-child .btn-down').disabled = true;
+  }
+
+  function renderStock(stock) {
+    let btnClass = parseFloat(stock.PercentChange) > 0 ? 'btn-green' : 'btn-red';
+    let btnData = stocksViewState === VIEW_STATES.DAILY_CHANGE ? stock.PercentChange : parseFloat(stock.Change).toFixed(2);
+    return `<li class="stock" data-symbol="${stock.Symbol}">
     <span class="stock-name">${stock.Symbol} (${stock.Name})</span>
     <div class="stock-data">
       <span>${parseFloat(stock.LastTradePriceOnly).toFixed(2)}</span>
       <button class="stock-data-btn ${btnClass}" data-id="stock-data-btn">${btnData}</button>
-      <div class="up-down-wrapper" data-symbol="${stock.Symbol}">
+      <div class="up-down-wrapper">
         <button class="nav-btn btn-up icon-arrow" data-direction="up" data-id="nav-btn"></button>
         <button class="nav-btn btn-down icon-arrow" data-direction="down" data-id="nav-btn"></button>
       </div>
     </div>
   </li>`
-}
+  }
 
-function disableButtons() {
-  document.querySelector('.stock:first-child .btn-up').disabled = true;
-  document.querySelector('.stock:last-child .btn-down').disabled = true;
-}
+  init();
 
-init();
+})();
+
 
 
 
